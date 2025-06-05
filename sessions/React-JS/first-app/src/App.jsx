879b28fb8cf5d5ccf1179a6842c3dc467eeb68e6
Sohail23-Dev <sohailshaikh778822@outlook.com>
2025-06-05 13:43:33 +0530

    First UI Updated

--- sessions/React-JS/first-app/src/App.jsx
+++ sessions/React-JS/first-app/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/NavBar/Nav.jsx";
 import NewHome from "./components/Home/NewHome.jsx";
 import Contact from "./components/Contact/Contact.jsx";
-import About from "./components/About/About.jsx";
 import Review from "./components/Review/Review.jsx";
+import Cart from "./components/Cart/Cart.jsx"
 import FAQ from "./components/FAQ/FAQ.jsx";
+import Login from "./components/Login/Login.jsx";
 
 const App = () => {
   
@@ -14,9 +15,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<NewHome />} />
         <Route path="/Contact" element={<Contact />} />
-        <Route path="/About" element={<About />} />
         <Route path="/Review" element={<Review />} />
+        <Route path="/Cart" element={<Cart />} />
         <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
